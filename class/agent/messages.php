a04a1cedadba05d1f5d2560ac6977b94797440db
Yann Bogdanovic <ianbogda@gmail.com>
2013-01-10 22:22:12 +0100

    - Fix filters on messages

--- class/agent/messages.php
+++ class/agent/messages.php
@@ -20,7 +20,39 @@ class agent_messages extends agent
 	$sqlOrderBy = 'ORDER BY `ReceivedAt` DESC',
 	$sqlLimit = 0,
 
-	$priorities = array( 'Emergency', 'Alert', 'Critical', 'Error', 'Warning', 'Notice', 'Informational', 'Debug');
+	$priorities = array( 'Emergency', 'Alert', 'Critical', 'Error', 'Warning', 'Notice', 'Informational', 'Debug'),
+
+	/* from http://techras.wordpress.com/2011/06/02/regex-replace-for-mysql/ */
+	$sqlRegexReplace = "DELIMITER 
+CREATE FUNCTION  `regex_replace`(pattern VARCHAR(1000),replacement VARCHAR(1000),original VARCHAR(1000))
+
+RETURNS VARCHAR(1000)
+DETERMINISTIC
+BEGIN 
+ DECLARE temp VARCHAR(1000); 
+ DECLARE ch VARCHAR(1); 
+ DECLARE i INT;
+ SET i = 1;
+ SET temp = '';
+ IF original REGEXP pattern THEN 
+  loop_label: LOOP 
+   IF i>CHAR_LENGTH(original) THEN
+    LEAVE loop_label;  
+   END IF;
+   SET ch = SUBSTRING(original,i,1);
+   IF NOT ch REGEXP pattern THEN
+    SET temp = CONCAT(temp,ch);
+   ELSE
+    SET temp = CONCAT(temp,replacement);
+   END IF;
+   SET i=i+1;
+  END LOOP;
+ ELSE
+  SET temp = original;
+ END IF;
+ RETURN temp;
+END$$
+DELIMITER ;";
 
 	function control()
 	{
@@ -59,9 +91,13 @@ class agent_messages extends agent
 					case 'facility' : $this->sqlSelect = "`Facility`";  break;
 					case 'input'    : $this->sqlSelect = "`SysLogTag`"; break;
 				}
-				$this->sqlGroupBy = "GROUP BY {$this->sqlSelect} ASC";
-				$this->sqlOrderBy = "ORDER BY {$this->sqlSelect} ASC";
-				$this->sqlSelect .= " AS source, COUNT(*) as `count`";
+				$this->sqlSelect = "regex_replace('\\\\\]|\\\\\[|:','',"
+					. ( 'input' == $this->get->__1__ ? "regex_replace('([0-9])',''," : "" )
+					. "{$this->sqlSelect})"
+					. ( 'input' == $this->get->__1__ ? ")" : "" )
+					. " AS source, COUNT(*) as `count`";
+				$this->sqlGroupBy = "GROUP BY source";
+				$this->sqlOrderBy = "ORDER BY `source` ASC";
 			}
 		}
 
@@ -70,6 +106,9 @@ class agent_messages extends agent
 
 	function compose($o)
 	{
+		//TODO check if udf exists
+//		$r = DB()->exec($this->sqlRegexReplace);
+
 		$sql = "SELECT {$this->sqlSelect}
 				FROM `SystemEvents`
 				WHERE {$this->sqlWhere}
