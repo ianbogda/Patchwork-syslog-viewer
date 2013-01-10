48b9f03cf22ac4664f526bc18b53b90d36abd810
ianbogda <ianbogda@gmail.com>
2013-01-10 17:49:40 +0100

    - Fix filters

--- class/agent/messages.php
+++ class/agent/messages.php
@@ -5,16 +5,20 @@ class agent_messages extends agent
 	public
 	
 	$get = array(
-		'__1__:c' => 'host|input|priority|facilityi',
-		'__2__:c' => -1
+		'__1__:c:host|input|priority|facility',
+		'__2__:c' => -1,
+		'p:i' => 0
 	);
 
 	protected
 
 	$results_per_page = 50,
-	$results_nb,
-	$filter = 1,
-	$groupBy = '',
+
+	$sqlSelect = 'ID, ReceivedAt, FromHost, SysLogTag, Priority, Facility, Message',
+	$sqlWhere = 1,
+	$sqlGroupBy = '',
+	$sqlOrderBy = 'ORDER BY `ReceivedAt` DESC',
+	$sqlLimit = 0,
 
 	$priorities = array( 'Emergency', 'Alert', 'Critical', 'Error', 'Warning', 'Notice', 'Informational', 'Debug');
 
@@ -26,10 +30,10 @@ class agent_messages extends agent
 		{
 			switch ($this->get->__1__)
 			{
-				case 'host'     : $this->filter = "`FromHost`";  break;
-				case 'input'    : $this->filter = "`SysLogTag`"; break;
-				case 'priority' : $this->filter = "`Priority`";  break;
-				case 'facility' : $this->filter = "`Facility`";  break;
+				case 'host'     : $this->sqlWhere = "`FromHost`";  break;
+				case 'input'    : $this->sqlWhere = "`SysLogTag`"; break;
+				case 'priority' : $this->sqlWhere = "`Priority`";  break;
+				case 'facility' : $this->sqlWhere = "`Facility`";  break;
 			}
 
 
@@ -39,35 +43,44 @@ class agent_messages extends agent
 				{
 					case 'host':
 					case 'priority' :
-					case 'facility' : $this->filter .= " = '%s'";     break;
-					case 'input'    : $this->filter .= " LIKE '%s%'"; break;
+					case 'facility' : $this->sqlWhere .= " = '%s'";     break;
+					case 'input'    : $this->sqlWhere .= " LIKE '%s%'"; break;
 				}
-				$this->filter = str_replace('%s', $this->get->__2__, $this->filter);
+				$this->sqlWhere = str_replace('%s', $this->get->__2__, $this->sqlWhere);
 			}
 			else
 			{
-				$this->filter = 1;
+				$this->sqlWhere = 1;
+
+				switch ($this->get->__1__)
+				{
+					case 'host'     : $this->sqlSelect = "`FromHost`";  break;
+					case 'priority' : $this->sqlSelect = "`Priority`";  break;
+					case 'facility' : $this->sqlSelect = "`Facility`";  break;
+					case 'input'    : $this->sqlSelect = "`SysLogTag`"; break;
+				}
+				$this->sqlGroupBy = "GROUP BY {$this->sqlSelect} ASC";
+				$this->sqlOrderBy = "ORDER BY {$this->sqlSelect} ASC";
+				$this->sqlSelect .= " AS source, COUNT(*) as `count`";
 			}
 		}
+
+		0 != $this->get->p || $this->sqlLimit = $this->get->p * $this->results_per_page;
 	}
 
 	function compose($o)
 	{
-		$sql = "SELECT
-					ID,
-					ReceivedAt,
-					FromHost,
-					SysLogTag,
-					Priority,
-					Facility,
-					Message
+		$sql = "SELECT {$this->sqlSelect}
 				FROM `SystemEvents`
-				WHERE {$this->filter}
-				ORDER BY `ReceivedAt` DESC";
+				WHERE {$this->sqlWhere}
+				{$this->sqlGroupBy}
+				{$this->sqlOrderBy}
+				LIMIT {$this->sqlLimit}, {$this->results_per_page}";
 
 		$o->messages = new loop_sql($sql);
-E($sql);
+
 		$o->results_per_page = $this->results_per_page;
+		$o->page  = $this->get->p + 1;
 
 		return $o;
 	}
