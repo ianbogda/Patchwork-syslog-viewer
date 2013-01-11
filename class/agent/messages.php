7ff54bb3bf39278909898c9328ed6d90c4bdd987
ianbogda <ianbogda@gmail.com>
2013-01-11 08:50:28 +0100

    - Fix typo

--- class/agent/messages.php
+++ class/agent/messages.php
@@ -23,36 +23,33 @@ class agent_messages extends agent
 	$priorities = array( 'Emergency', 'Alert', 'Critical', 'Error', 'Warning', 'Notice', 'Informational', 'Debug'),
 
 	/* from http://techras.wordpress.com/2011/06/02/regex-replace-for-mysql/ */
-	$sqlRegexReplace = "DELIMITER 
-CREATE FUNCTION  `regex_replace`(pattern VARCHAR(1000),replacement VARCHAR(1000),original VARCHAR(1000))
-
-RETURNS VARCHAR(1000)
-DETERMINISTIC
-BEGIN 
- DECLARE temp VARCHAR(1000); 
- DECLARE ch VARCHAR(1); 
- DECLARE i INT;
- SET i = 1;
- SET temp = '';
- IF original REGEXP pattern THEN 
-  loop_label: LOOP 
-   IF i>CHAR_LENGTH(original) THEN
-    LEAVE loop_label;  
-   END IF;
-   SET ch = SUBSTRING(original,i,1);
-   IF NOT ch REGEXP pattern THEN
-    SET temp = CONCAT(temp,ch);
-   ELSE
-    SET temp = CONCAT(temp,replacement);
-   END IF;
-   SET i=i+1;
-  END LOOP;
- ELSE
-  SET temp = original;
- END IF;
- RETURN temp;
-END$$
-DELIMITER ;";
+	$sqlRegexReplace = "CREATE FUNCTION  `regex_replace`(pattern VARCHAR(1000),replacement VARCHAR(1000),original VARCHAR(1000))
+						RETURNS VARCHAR(1000)
+						DETERMINISTIC
+						BEGIN 
+							DECLARE temp VARCHAR(1000); 
+							DECLARE ch VARCHAR(1); 
+							DECLARE i INT;
+							SET i = 1;
+							SET temp = '';
+							IF original REGEXP pattern THEN 
+								loop_label: LOOP 
+									IF i>CHAR_LENGTH(original) THEN
+										LEAVE loop_label;  
+									END IF;
+									SET ch = SUBSTRING(original,i,1);
+									IF NOT ch REGEXP pattern THEN
+										SET temp = CONCAT(temp,ch);
+									ELSE
+										SET temp = CONCAT(temp,replacement);
+									END IF;
+									SET i=i+1;
+								END LOOP;
+							ELSE
+								SET temp = original;
+							END IF;
+							RETURN temp;
+						END;";
 
 	function control()
 	{
@@ -107,7 +104,7 @@ DELIMITER ;";
 	function compose($o)
 	{
 		//TODO check if udf exists
-//		$r = DB()->exec($this->sqlRegexReplace);
+		$r = DB()->exec($this->sqlRegexReplace);
 
 		$sql = "SELECT {$this->sqlSelect}
 				FROM `SystemEvents`
